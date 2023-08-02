781718ef87ede7f504b81f1b8e9357e80d033159
root <root@cnode2.pvetest.com>
2023-08-02 11:29:21 +0800

    new file:   scripts/clean.sh

---
+++ scripts/clean.sh
@@ -0,0 +1,9 @@
+rm debian/pve-headers-* -rf
+rm debian/pve-kernel-* -rf
+rm .*_mark -rf
+rm debian/autoreconf* -rf
+rm debian/linux-tool* -rf
+rm debian/.debhelper/ -rf
+rm debian/SOURCE -rf
+rm debian/debhelper-build-stamp -rf
+rm debian/files -rf
