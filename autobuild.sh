c0288c68388ff73736b8aa4d2736d2fe8b99519e
jiangcuo <jiangcuo@bingsin.com>
2023-10-31 23:30:03 +0800

    fix zfs module

--- autobuild.sh
+++ autobuild.sh
@@ -2,6 +2,7 @@
 sudo apt install devscripts  rsync -y
 yes | sudo mk-build-deps --install --remove
 git submodule update --init --depth=1
+rm build -rf
 mkdir build && rsync -ra * build/
 cd build
 dpkg-buildpackage -b -us -uc
