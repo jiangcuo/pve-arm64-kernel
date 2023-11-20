1631641800448f6eaae258f7f34ce57c9a4f69e9
jiangcuo <jiangcuo@bingsin.com>
2023-11-20 11:02:08 +0800

    Update to Linux 6.5.11

--- autobuild.sh
+++ autobuild.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 sudo apt install devscripts  rsync -y
-yes | sudo mk-build-deps --install --remove
-git submodule update --init --depth=1
+#git submodule update --init --depth=1
 rm build -rf
 mkdir build && rsync -ra * build/
 cd build
+debian/rules debian/control
+yes | sudo mk-build-deps --install --remove
 dpkg-buildpackage -b -us -uc
