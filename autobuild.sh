054983ca1baf59cb93a17c6549832574c1304405
jiangcuo <jiangcuo@bingsin.com>
2024-06-09 10:17:50 +0800

    Optimize kernel build

--- autobuild.sh
+++ autobuild.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 git submodule update --init --depth=1
 debian/rules debian/control
-sudo apt install devscripts  rsync -y
-yes | sudo mk-build-deps --install --remove
+apt install devscripts  rsync -y
+yes |mk-build-deps --install --remove
 rm build -rf
 mkdir build && rsync -ra * .git build/
 cd build
