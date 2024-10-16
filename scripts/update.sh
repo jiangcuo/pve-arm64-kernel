3f28d141d74118e9412a500793f32b52d4f634f1
jiangcuo <jiangcuo@bingsin.com>
2024-10-16 09:00:22 +0800

    Create this branch

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pkg_version=`date +%Y%m%d`
 
-git --git-dir linux/.git checkout FETCH_HEAD
+#git --git-dir linux/.git checkout FETCH_HEAD
 
 echo "update debchangelog"
-debchange -v 6.6-$pkg_version -D openeuler --force-distribution -U -M "Update to Linux $pkg_version"
+debchange -v 6.6-$pkg_version -D phytium --force-distribution -U -M "Update to Linux $pkg_version"
