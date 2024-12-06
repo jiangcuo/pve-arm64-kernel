5960a180eff828e8a496275718daad585e484b5e
jiangcuo <jiangcuo@bingsin.com>
2024-12-06 14:38:54 +0800

    Rename kernel name.

--- debian/rules.d/common.mk
+++ debian/rules.d/common.mk
@@ -8,14 +8,15 @@ PKG_RELEASE = $(shell ./scripts/version.sh -r)
 PKG_DATE := $(shell dpkg-parsechangelog -SDate)
 PKG_DATE_UTC_ISO := $(shell date -u -d '$(PKG_DATE)' +%Y-%m-%d)
 PKG_GIT_VERSION := $(shell git rev-parse HEAD)
+PKG_VERSION := $(shell )
 
 # Build settings
 PVE_KERNEL_CC ?= ${CC}
 PVE_ZFS_CC ?= ${CC}
 
 ### Debian package names
-EXTRAVERSION=-${DEB_DISTRIBUTION}
-KVNAME=${KERNEL_VER}-${DEB_DISTRIBUTION}
+EXTRAVERSION=-${PKG_RELEASE}-${DEB_DISTRIBUTION}
+KVNAME=${KERNEL_VER}-${PKG_RELEASE}-${DEB_DISTRIBUTION}
 
 PVE_KERNEL_PKG=pve-kernel-${KVNAME}
 PVE_HEADER_PKG=pve-headers-${KVNAME}
