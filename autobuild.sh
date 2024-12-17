03969eb0ccc8c9b2a06da01a8e9312d08e251b55
jiangcuo <jiangcuo@bingsin.com>
2024-12-17 14:13:32 +0800

    update linux to 6.1.119

--- autobuild.sh
+++ autobuild.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-git submodule update --init --depth=1
 debian/rules debian/control
-sudo apt install devscripts  rsync -y
-yes | sudo mk-build-deps --install --remove
 rm build -rf
 mkdir build && rsync -ra * .git build/
 cd build
